8ac3be3793ce9102d1b7da0b23034795a212a0de
swift777 <geshyk_777@geshyk-777s-MacBook-Air.local>
2014-06-06 02:29:09 +0400

    1234

---
+++ Hello World/src/Human.java
@@ -0,0 +1,10 @@
+
+public class Human 
+{
+	void go() {
+		System.out.print(" 5 букв -> ");
+	}
+	{
+		System.out.print(" 5 букв -> ");	
+	}
+};
---
+++ Hello World/src/hello.java
@@ -0,0 +1,14 @@
+//implements.class Human.java 
+public class hello
+{
+	
+
+	public void main(String[] args) 
+	{
+	
+		
+		System.out.print("‚ведите  5 букв -> ");
+
+	}
+
+}
